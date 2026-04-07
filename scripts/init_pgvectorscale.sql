bd463d8a663aba48b9a64db62aeef412781dece1
Unknown Flying Oleg <barss.mine@yandex.ru>
2026-04-08 02:14:47 +0300

    fix: исправлен файл инициализации pgvectorscale

--- scripts/init_pgvectorscale.sql
+++ scripts/init_pgvectorscale.sql
@@ -1,10 +1,21 @@
 -- Инициализация pgvectorscale
 -- Этот скрипт выполняется при первом запуске контейнера PostgreSQL
 
--- Создание расширения vectorscale
-CREATE EXTENSION IF NOT EXISTS vectorscale CASCADE;
+-- Попытка включить vectorscale, если он установлен
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1 FROM pg_available_extensions WHERE name = 'vectorscale'
+    ) THEN
+        CREATE EXTENSION IF NOT EXISTS vectorscale CASCADE;
+        RAISE NOTICE 'pgvectorscale подключен';
+    ELSE
+        RAISE NOTICE 'pgvectorscale не найден, используем только pgvector';
+        CREATE EXTENSION IF NOT EXISTS vector CASCADE;
+    END IF;
+END $$;
 
--- Создание пользователя для бенчмарка (опционально)
+-- Создание пользователя
 DO $$
 BEGIN
     IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'benchmark_user') THEN
@@ -12,8 +23,5 @@ BEGIN
     END IF;
 END $$;
 
--- Предоставление прав
 GRANT ALL ON DATABASE rag_benchmark TO benchmark_user;
 GRANT ALL ON SCHEMA public TO benchmark_user;
-
-SELECT 'pgvectorscale initialized' AS status;
